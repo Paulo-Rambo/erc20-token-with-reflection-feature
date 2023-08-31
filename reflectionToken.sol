19cfce7f7454493fa31a796e247dc490ab2d2aa3
Paulo Vitor <patoassado@gmail.com>
2023-08-31 18:26:39 -0300

    fix bug

--- reflectionToken.sol
+++ reflectionToken.sol
@@ -14,14 +14,41 @@ interface IERC20 {
     event Approval(address indexed owner, address indexed spender, uint256 value);
 }
 
-interface IERC20Metadata is IERC20 {
 
+// File: openzeppelin-solidity/contracts/token/ERC20/ERC20.sol
+
+/**
+ * @title Standard ERC20 token
+ *
+ * @dev Implementation of the basic standard token.
+ * https://github.com/ethereum/EIPs/blob/master/EIPS/eip-20.md
+ * Originally based on code by FirstBlood:
+ * https://github.com/Firstbloodio/token/blob/master/smart_contract/FirstBloodToken.sol
+ *
+ * This implementation emits additional Approval events, allowing applications to reconstruct the allowance status for
+ * all accounts just by listening to said events. Note that this isn't required by the specification, and other
+ * compliant implementations may not do it.
+ */
+
+interface IERC20Metadata is IERC20 {
+    /**
+     * @dev Returns the name of the token.
+     */
     function name() external view returns (string memory);
 
+    /**
+     * @dev Returns the symbol of the token.
+     */
     function symbol() external view returns (string memory);
 
+    /**
+     * @dev Returns the decimals places of the token.
+     */
     function decimals() external view returns (uint8);
     
+    /**
+     * @dev Returns the %reflectios.
+     */
     function reflections() external view returns (uint256);
 }
 
@@ -40,32 +67,59 @@ abstract contract Ownable is Context {
 
     event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
 
+    /**
+     * @dev Initializes the contract setting the deployer as the initial owner.
+     */
     constructor() {
         _transferOwnership(_msgSender());
     }
 
+    /**
+     * @dev Throws if called by any account other than the owner.
+     */
     modifier onlyOwner() {
         _checkOwner();
         _;
     }
 
+    /**
+     * @dev Returns the address of the current owner.
+     */
     function owner() public view virtual returns (address) {
         return _owner;
     }
 
+    /**
+     * @dev Throws if the sender is not the owner.
+     */
     function _checkOwner() internal view virtual {
         require(owner() == _msgSender(), "Ownable: caller is not the owner");
     }
 
+    /**
+     * @dev Leaves the contract without owner. It will not be possible to call
+     * `onlyOwner` functions. Can only be called by the current owner.
+     *
+     * NOTE: Renouncing ownership will leave the contract without an owner,
+     * thereby disabling any functionality that is only available to the owner.
+     */
     function renounceOwnership() public virtual onlyOwner {
         _transferOwnership(address(0));
     }
 
+    /**
+     * @dev Transfers ownership of the contract to a new account (`newOwner`).
+     * Can only be called by the current owner.
+     */
     function transferOwnership(address newOwner) public virtual onlyOwner {
         require(newOwner != address(0), "Ownable: new owner is the zero address");
         _transferOwnership(newOwner);
     }
 
+    /**
+     * @dev Transfers ownership of the contract to a new account (`newOwner`).
+     * Internal function without access restriction.
+     */
     function _transferOwnership(address newOwner) internal virtual {
         address oldOwner = _owner;
         _owner = newOwner;
@@ -93,19 +147,45 @@ contract ERC20 is Context, IERC20, IERC20Metadata, Ownable {
     bool private _reflections;
     uint256 private _reflectionsRange;
 
+    /**
+     * @dev Sets the values for {name} and {symbol}.
+     *
+     * All two of these values are immutable: they can only be set once during
+     * construction.
+     */
     constructor(string memory name_, string memory symbol_) {
         _name = name_;
         _symbol = symbol_;
     }
 
+    /**
+     * @dev Returns the name of the token.
+     */
     function name() public view virtual override returns (string memory) {
         return _name;
     }
 
+    /**
+     * @dev Returns the symbol of the token, usually a shorter version of the
+     * name.
+     */
     function symbol() public view virtual override returns (string memory) {
         return _symbol;
     }
 
+    /**
+     * @dev Returns the number of decimals used to get its user representation.
+     * For example, if `decimals` equals `2`, a balance of `505` tokens should
+     * be displayed to a user as `5.05` (`505 / 10 ** 2`).
+     *
+     * Tokens usually opt for a value of 18, imitating the relationship between
+     * Ether and Wei. This is the default value returned by this function, unless
+     * it's overridden.
+     *
+     * NOTE: This information is only used for _display_ purposes: it in
+     * no way affects any of the arithmetic of the contract, including
+     * {IERC20-balanceOf} and {IERC20-transfer}.
+     */
     function decimals() public view virtual override returns (uint8) {
         return 18;
     }
@@ -127,30 +207,73 @@ contract ERC20 is Context, IERC20, IERC20Metadata, Ownable {
         }
     }
 
+    /**
+     * @dev See {IERC20-totalSupply}.
+     */
     function totalSupply() public view virtual override returns (uint256) {
         return _totalSupply;
     }
 
+    /**
+     * @dev See {IERC20-balanceOf}.
+     */
     function balanceOf(address account) public view virtual override returns (uint256) {
         return _balances[account];
     }
 
+    /**
+     * @dev See {IERC20-transfer}.
+     *
+     * Requirements:
+     *
+     * - `to` cannot be the zero address.
+     * - the caller must have a balance of at least `amount`.
+     */
     function transfer(address to, uint256 amount) public virtual override returns (bool) {
         address owner = _msgSender();
         _transfer(owner, to, amount);
         return true;
     }
 
+    /**
+     * @dev See {IERC20-allowance}.
+     */
     function allowance(address owner, address spender) public view virtual override returns (uint256) {
         return _allowances[owner][spender];
     }
 
+    /**
+     * @dev See {IERC20-approve}.
+     *
+     * NOTE: If `amount` is the maximum `uint256`, the allowance is not updated on
+     * `transferFrom`. This is semantically equivalent to an infinite approval.
+     *
+     * Requirements:
+     *
+     * - `spender` cannot be the zero address.
+     */
     function approve(address spender, uint256 amount) public virtual override returns (bool) {
         address owner = _msgSender();
         _approve(owner, spender, amount);
         return true;
     }
 
+    /**
+     * @dev See {IERC20-transferFrom}.
+     *
+     * Emits an {Approval} event indicating the updated allowance. This is not
+     * required by the EIP. See the note at the beginning of {ERC20}.
+     *
+     * NOTE: Does not update the allowance if the current allowance
+     * is the maximum `uint256`.
+     *
+     * Requirements:
+     *
+     * - `from` and `to` cannot be the zero address.
+     * - `from` must have a balance of at least `amount`.
+     * - the caller must have allowance for ``from``'s tokens of at least
+     * `amount`.
+     */
     function transferFrom(address from, address to, uint256 amount) public virtual override returns (bool) {
         address spender = _msgSender();
         _spendAllowance(from, spender, amount);
@@ -158,12 +281,38 @@ contract ERC20 is Context, IERC20, IERC20Metadata, Ownable {
         return true;
     }
 
+    /**
+     * @dev Atomically increases the allowance granted to `spender` by the caller.
+     *
+     * This is an alternative to {approve} that can be used as a mitigation for
+     * problems described in {IERC20-approve}.
+     *
+     * Emits an {Approval} event indicating the updated allowance.
+     *
+     * Requirements:
+     *
+     * - `spender` cannot be the zero address.
+     */
     function increaseAllowance(address spender, uint256 addedValue) public virtual returns (bool) {
         address owner = _msgSender();
         _approve(owner, spender, allowance(owner, spender) + addedValue);
         return true;
     }
 
+    /**
+     * @dev Atomically decreases the allowance granted to `spender` by the caller.
+     *
+     * This is an alternative to {approve} that can be used as a mitigation for
+     * problems described in {IERC20-approve}.
+     *
+     * Emits an {Approval} event indicating the updated allowance.
+     *
+     * Requirements:
+     *
+     * - `spender` cannot be the zero address.
+     * - `spender` must have allowance for the caller of at least
+     * `subtractedValue`.
+     */
     function decreaseAllowance(address spender, uint256 subtractedValue) public virtual returns (bool) {
         address owner = _msgSender();
         uint256 currentAllowance = allowance(owner, spender);
@@ -175,6 +324,20 @@ contract ERC20 is Context, IERC20, IERC20Metadata, Ownable {
         return true;
     }
 
+    /**
+     * @dev Moves `amount` of tokens from `from` to `to`.
+     *
+     * This internal function is equivalent to {transfer}, and can be used to
+     * e.g. implement automatic token fees, slashing mechanisms, etc.
+     *
+     * Emits a {Transfer} event.
+     *
+     * Requirements:
+     *
+     * - `from` cannot be the zero address.
+     * - `to` cannot be the zero address.
+     * - `from` must have a balance of at least `amount`.
+     */
     function _transfer(address from, address to, uint256 amount) internal virtual {
         require(from != address(0), "ERC20: transfer from the zero address");
         require(to != address(0), "ERC20: transfer to the zero address");
@@ -184,11 +347,11 @@ contract ERC20 is Context, IERC20, IERC20Metadata, Ownable {
 
         uint256 fromBalance = _balances[from];
         require(fromBalance >= amount, "ERC20: transfer amount exceeds balance");
-        uint256 newAmount;
+        uint256 newAmount = amount;
 
         if(_reflections == true && _reflectionAdresses[from] == true){
             require(_reflections, "No reflection active");
-            require(_reflectionAdresses[to], "No reflection address active");
+            require(_reflectionAdresses[from], "No reflection address active");
             reflectionAmount = (amount*_reflectionsRange)/100;
             _totalSupply -= reflectionAmount;
             newAmount = amount-reflectionAmount;
@@ -205,6 +368,15 @@ contract ERC20 is Context, IERC20, IERC20Metadata, Ownable {
         _afterTokenTransfer(from, to, newAmount);
     }
 
+    /** @dev Creates `amount` tokens and assigns them to `account`, increasing
+     * the total supply.
+     *
+     * Emits a {Transfer} event with `from` set to the zero address.
+     *
+     * Requirements:
+     *
+     * - `account` cannot be the zero address.
+     */
     function _mint(address account, uint256 amount) internal virtual {
         require(account != address(0), "ERC20: mint to the zero address");
 
@@ -220,6 +392,17 @@ contract ERC20 is Context, IERC20, IERC20Metadata, Ownable {
         _afterTokenTransfer(address(0), account, amount);
     }
 
+    /**
+     * @dev Destroys `amount` tokens from `account`, reducing the
+     * total supply.
+     *
+     * Emits a {Transfer} event with `to` set to the zero address.
+     *
+     * Requirements:
+     *
+     * - `account` cannot be the zero address.
+     * - `account` must have at least `amount` tokens.
+     */
     function _burn(address account, uint256 amount) internal virtual {
         require(account != address(0), "ERC20: burn from the zero address");
 
@@ -238,6 +421,19 @@ contract ERC20 is Context, IERC20, IERC20Metadata, Ownable {
         _afterTokenTransfer(account, address(0), amount);
     }
 
+    /**
+     * @dev Sets `amount` as the allowance of `spender` over the `owner` s tokens.
+     *
+     * This internal function is equivalent to `approve`, and can be used to
+     * e.g. set automatic allowances for certain subsystems, etc.
+     *
+     * Emits an {Approval} event.
+     *
+     * Requirements:
+     *
+     * - `owner` cannot be the zero address.
+     * - `spender` cannot be the zero address.
+     */
     function _approve(address owner, address spender, uint256 amount) internal virtual {
         require(owner != address(0), "ERC20: approve from the zero address");
         require(spender != address(0), "ERC20: approve to the zero address");
@@ -246,6 +442,14 @@ contract ERC20 is Context, IERC20, IERC20Metadata, Ownable {
         emit Approval(owner, spender, amount);
     }
 
+    /**
+     * @dev Updates `owner` s allowance for `spender` based on spent `amount`.
+     *
+     * Does not update the allowance amount in case of infinite allowance.
+     * Revert if not enough allowance is available.
+     *
+     * Might emit an {Approval} event.
+     */
     function _spendAllowance(address owner, address spender, uint256 amount) internal virtual {
         uint256 currentAllowance = allowance(owner, spender);
         if (currentAllowance != type(uint256).max) {
@@ -256,16 +460,53 @@ contract ERC20 is Context, IERC20, IERC20Metadata, Ownable {
         }
     }
 
+    /**
+     * @dev Hook that is called before any transfer of tokens. This includes
+     * minting and burning.
+     *
+     * Calling conditions:
+     *
+     * - when `from` and `to` are both non-zero, `amount` of ``from``'s tokens
+     * will be transferred to `to`.
+     * - when `from` is zero, `amount` tokens will be minted for `to`.
+     * - when `to` is zero, `amount` of ``from``'s tokens will be burned.
+     * - `from` and `to` are never both zero.
+     *
+     * To learn more about hooks, head to xref:ROOT:extending-contracts.adoc#using-hooks[Using Hooks].
+     */
     function _beforeTokenTransfer(address from, address to, uint256 amount) internal virtual {}
 
+    /**
+     * @dev Hook that is called after any transfer of tokens. This includes
+     * minting and burning.
+     *
+     * Calling conditions:
+     *
+     * - when `from` and `to` are both non-zero, `amount` of ``from``'s tokens
+     * has been transferred to `to`.
+     * - when `from` is zero, `amount` tokens have been minted for `to`.
+     * - when `to` is zero, `amount` of ``from``'s tokens have been burned.
+     * - `from` and `to` are never both zero.
+     *
+     * To learn more about hooks, head to xref:ROOT:extending-contracts.adoc#using-hooks[Using Hooks].
+     */
     function _afterTokenTransfer(address from, address to, uint256 amount) internal virtual {}
 }
 
+// File: openzeppelin-solidity/contracts/access/Roles.sol
+
+/**
+ * @title Roles
+ * @dev Library for managing addresses assigned to a Role.
+ */
 library Roles {
     struct Role {
         mapping (address => bool) bearer;
     }
 
+    /**
+     * @dev give an account access to this role
+     */
     function add(Role storage role, address account) internal {
         require(account != address(0));
         require(!has(role, account));
@@ -273,6 +514,9 @@ library Roles {
         role.bearer[account] = true;
     }
 
+    /**
+     * @dev remove an account's access to this role
+     */
     function remove(Role storage role, address account) internal {
         require(account != address(0));
         require(has(role, account));
@@ -280,6 +524,10 @@ library Roles {
         role.bearer[account] = false;
     }
 
+    /**
+     * @dev check if an account has this role
+     * @return bool
+     */
     function has(Role storage role, address account) internal view returns (bool) {
         require(account != address(0));
         return role.bearer[account];
@@ -330,45 +578,94 @@ abstract contract PauserRole {
 
 // File: openzeppelin-solidity/contracts/lifecycle/Pausable.sol
 
+/**
+ * @title Pausable
+ * @dev Base contract which allows children to implement an emergency stop mechanism.
+ */
 abstract contract Pausable is Context {
-
+    /**
+     * @dev Emitted when the pause is triggered by `account`.
+     */
     event Paused(address account);
 
+    /**
+     * @dev Emitted when the pause is lifted by `account`.
+     */
     event Unpaused(address account);
 
     bool private _paused;
 
+    /**
+     * @dev Initializes the contract in unpaused state.
+     */
     constructor() {
         _paused = false;
     }
 
+    /**
+     * @dev Modifier to make a function callable only when the contract is not paused.
+     *
+     * Requirements:
+     *
+     * - The contract must not be paused.
+     */
     modifier whenNotPaused() {
         _requireNotPaused();
         _;
     }
 
+    /**
+     * @dev Modifier to make a function callable only when the contract is paused.
+     *
+     * Requirements:
+     *
+     * - The contract must be paused.
+     */
     modifier whenPaused() {
         _requirePaused();
         _;
     }
 
+    /**
+     * @dev Returns true if the contract is paused, and false otherwise.
+     */
     function paused() public view virtual returns (bool) {
         return _paused;
     }
 
+    /**
+     * @dev Throws if the contract is paused.
+     */
     function _requireNotPaused() internal view virtual {
         require(!paused(), "Pausable: paused");
     }
 
+    /**
+     * @dev Throws if the contract is not paused.
+     */
     function _requirePaused() internal view virtual {
         require(paused(), "Pausable: not paused");
     }
 
+    /**
+     * @dev Triggers stopped state.
+     *
+     * Requirements:
+     *
+     * - The contract must not be paused.
+     */
     function _pause() internal virtual whenNotPaused {
         _paused = true;
         emit Paused(_msgSender());
     }
 
+    /**
+     * @dev Returns to normal state.
+     *
+     * Requirements:
+     *
+     * - The contract must be paused.
+     */
     function _unpause() internal virtual whenPaused {
         _paused = false;
         emit Unpaused(_msgSender());
